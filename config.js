193b4250554529e13924d39904b5f4fed933f5fb
Benjamin Humeau <benjamin@pakitow.eu>
2015-10-27 11:41:26 +0100

    Changement de port

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ var config  = {};
 config.db   = {};
 config.web  = {};
 
-config.web.port     = process.env.PORT || 8080;
+config.web.port     = process.env.PORT || 1337;
 config.db.host      = 'pakilow.eu';
 config.db.user      = 'root';
 config.db.password  = 'test';
